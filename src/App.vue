4e728751947543c4c12912690753b54f09a4c2f4
zyh <137047551@qq.com>
2022-02-06 20:12:04 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <dax1/>
+</template>
+
+<script>
+import dax1 from './components/dax1.vue'
+
+export default {
+  name: 'App',
+  components: {
+    dax1
+  }
+}
+</script>
