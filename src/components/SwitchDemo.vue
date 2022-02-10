0c55276e0dc9c6a08fca5fde56b5d64e088b9693
zyh <137047551@qq.com>
2022-02-10 16:39:59 +0800

    switch可以接受外部value和input事件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <Switch />
+    <Switch :value="y" @input="y = $event"/>
   </div>
 </template>
 
-<script>
+<script lant="ts">
 import Switch from '../lib/Switch.vue'
+import {ref} from 'vue'
+
 export default {
   components: {
     Switch
+  },
+  setup() {
+    const y = ref(true)
+    return {y}
   }
 }
 </script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,19 +1,21 @@
 <template>
-  <button :class="{checked:checked}" @click="toggle">
+  <button :class="{checked:value}" @click="toggle">
     <span></span>
   </button>
 </template>
 
-<script>
+<script lang="ts">
 import {ref} from 'vue'
 
 export default {
-  setup() {
-    const checked = ref(false)
+  props: {
+    value: Boolean
+  },
+  setup(props, context) {
     const toggle = () => {
-      checked.value = !checked.value
+      context.emit('input', !props.value)
     }
-    return {checked, toggle}
+    return {toggle}
   }
 }
 </script>
