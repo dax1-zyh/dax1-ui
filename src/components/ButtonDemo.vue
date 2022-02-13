1ce46157a6ad08e909a26f00c8f6dfda4966b744
zyh <137047551@qq.com>
2022-02-13 21:10:19 +0800

    将父元素属附件性绑将父组件属性绑定到子组件非根元素上

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -4,14 +4,20 @@
   </div>
   <h1>示例1</h1>
   <div>
-    <Button>你好</Button>
+    <Button @click="onClick">你好</Button>
   </div>
 </template>
 
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
-  components: {Button}
+  components: {Button},
+  setup() {
+    const onClick = () => {
+      console.log('hi')
+    }
+    return {onClick}
+  }
 }
 
 </script>
