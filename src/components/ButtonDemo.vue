a73db97cafaac866d34695f0c6c02de401898137
zyh <137047551@qq.com>
2022-02-13 20:28:36 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,104 +1,17 @@
 <template>
   <div>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
-    <p>Button 的文档</p>
+    Button 示例
+  </div>
+  <h1>示例1</h1>
+  <div>
+    <Button>你好</Button>
   </div>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+export default {
+  components: {Button}
+}
+
+</script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -44,10 +44,11 @@ span {
 
 button.checked {
   background: blue;
+
+  > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
 }
 
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
 
 </style>
